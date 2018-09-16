7b6ffb6a8099642749dc95acef7208f6c394c9f8
Weizhong Li <weizhongli1987@gmail.com>
2018-09-15 22:28:13 -0700

    NG-Omics-microbiome.py qc add qc.txt

--- workflow-examples/NG-Omics-microbiome.py
+++ workflow-examples/NG-Omics-microbiome.py
@@ -66,6 +66,13 @@ perl -e '$i=0; while(<>){ if (/^@/) {$i++;  print ">Sample|$SAMPLE|$i ", substr(
 
 wait
 rm -f $SELF/R1.fq $SELF/R2.fq $SELF/R1-s.fq $SELF/R2-s.fq
+
+NUM_reads_total=$(grep "Input Read Pairs"  $SELF/qc.stderr | cut -f 4 -d " ")
+NUM_reads_pass=$(grep "Input Read Pairs"  $SELF/qc.stderr | cut -f 7 -d " ")
+
+echo -e "Total_reads\\t$NUM_reads_total" > $SELF/qc.txt
+echo -e "QC_reads\\t$NUM_reads_pass" >> $SELF/qc.txt
+
 '''
 }
 
