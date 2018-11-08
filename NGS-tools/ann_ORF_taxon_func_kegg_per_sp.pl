7c5cc59e462cef1305e1737410f855645e1b1d32
Weizhong Li <weizhongli1987@gmail.com>
2018-11-08 11:14:08 -0800

    ann_ORF_taxon_func_kegg_per_sp.pl, allows other taxon ranks

--- NGS-tools/ann_ORF_taxon_func_kegg_per_sp.pl
+++ NGS-tools/ann_ORF_taxon_func_kegg_per_sp.pl
@@ -20,7 +20,7 @@ require "$script_dir/ann_local.pl";
 # these .keg file are from KEGG database /kegg/kegg-brite/ko/
 
 use Getopt::Std;
-getopts("i:k:a:o:e:d:s:t:r:c:",\%opts);
+getopts("i:k:a:o:e:d:s:t:r:c:K:",\%opts);
 die usage() unless ($opts{i} and $opts{k} and $opts{o} and $opts{r});
 
 my $ORF_ann_file = $opts{i}; #### blast alignment file in m8 format
@@ -31,7 +31,7 @@ my $ref_KOs      = $opts{r}; #### single-copy house keeping gene
 my $tmp_dir      = "$output.dir.$$";
 my $ref_KO_cov_cutoff = $opts{c}; #### skip species unless with enough ref hits
    $ref_KO_cov_cutoff = 0.3 unless ($ref_KO_cov_cutoff);
-
+my $key_col      = $opts{K}; $key_col =9 unless(defined($key_col));
 
 my ($i, $j, $k, $ll, $cmd);
 my $worker_script = "$script_dir/ann_ORF_taxon_func_kegg.pl -i $tmp_dir/in.txt -d $ORF_depth -k $keg_file -r $ref_KOs -o $tmp_dir/out"; 
@@ -52,64 +52,48 @@ my $num_ref_ko = scalar keys %single_copy_KOs;
 my $num_ref_ko_cutoff = $num_ref_ko * $ref_KO_cov_cutoff;
 
 my $last_sp = "";
-my $last_sp_name = "";
 my $orf_ann = "";
 my %KO_hits = ();
 
+my %org_2_txt;
+my %org_2_KO;
+my $col_name = "species";
+
 open(TMP, $ORF_ann_file) || die "can not open $ORF_ann_file";
 while($ll=<TMP>) {
-  next if ($ll =~ /^#/);
+  if ($ll =~ /^#/) {
+    $col_name = (split(/\t/,$ll))[$key_col];
+    next;
+  }
+
   my @lls = split(/\t/, $ll);
-  my $this_sp = $lls[0];
-  my $this_sp_name = $lls[9];
-
-  if (($this_sp ne $last_sp) and $orf_ann) {
-    if (scalar keys %KO_hits >= $num_ref_ko_cutoff) {
-      if ($last_sp =~ /\d+/) {
-        run_this($orf_ann, $last_sp, $last_sp_name);
-      }
-      else {
-        print STDERR "not valid species, $last_sp $last_sp_name\n";
-      }
-    } 
-    else {
-        my $n = scalar keys %KO_hits;
-        print STDERR "not enough ref KO hits ($n) for species, $last_sp $last_sp_name\n";
-    }
-    %KO_hits = ();
-    $orf_ann = "";
-  }
-  $orf_ann .= $ll;
-
+  my $this_sp = $lls[$key_col];
   my $KO = $lls[11+7]; #### if hit KO
-  $KO_hits{$KO} = 1 if (($KO =~ /^K\d+/) and $single_copy_KOs{$KO});
-  $last_sp = $this_sp;
-  $last_sp_name = $this_sp_name;
-} 
-  #### last block
-  if ($last_sp and $orf_ann) {
-    if (scalar keys %KO_hits >= $num_ref_ko_cutoff) {
-      if ($last_sp =~ /\d+/) {
-        run_this($orf_ann, $last_sp, $last_sp_name);
-      }
-      else {
-        print STDERR "not valid species, $last_sp $last_sp_name\n";
-      }
-    } 
-    else {
-        my $n = scalar keys %KO_hits;
-        print STDERR "not enough ref KO hits ($n) for species, $last_sp $last_sp_name\n";
-    }
+  $org_2_txt{$this_sp} .= $ll;
+  $org_2_KO{$this_sp}{$KO} = 1 if (($KO =~ /^K\d+/) and $single_copy_KOs{$KO});
+}
+foreach $i (keys %org_2_txt) {
+  if (not ($i =~ /\d+/)) {
+    print STDERR "not valid species, $i\n";
+    next;
   }
+  my $n = scalar keys %{ $org_2_KO{$i} };
+  if ($n >= $num_ref_ko_cutoff) {
+    run_this($org_2_txt{$i}, $i, $col_name);
+  }
+  else {
+    print STDERR "not enough ref KO hits ($n) for species, $i\n";
+  }
+}
 close(TMP);
 
 $cmd = `rm -rf $tmp_dir`;
 
 
 sub run_this {
-  my ($txt, $sp, $sp_name) = @_;
+  my ($txt, $sp, $col_name) = @_;
   my ($i, $j, $k, $ll, $cmd);
-  $sp_name =~ s/[^\w| ]/_/g;
+  $sp =~ s/[^\w| ]/_/g;
 
   open(OUT, "> $tmp_dir/in.txt") || die "can not write to $tmp_dir/in.txt";
   print OUT $txt;
@@ -125,9 +109,9 @@ sub run_this {
     $j = $i; 
     $j =~ s/^out.//; $j = "$output.$j"; #### $j is target output
     if (not -e $j) {
-      $cmd = `head -n 1 $tmp_dir/$i | sed "s/^/#Species\\tSpecies_name\\t/" > $j`;
+      $cmd = `head -n 1 $tmp_dir/$i | sed "s/^/#$col_name\\t/" > $j`;
     }
-    $cmd = `grep -v "^#" $tmp_dir/$i | sed "s/^/$sp\\t$sp_name\\t/" >> $j`;
+    $cmd = `grep -v "^#" $tmp_dir/$i | sed "s/^/$sp\\t/" >> $j`;
   }
 }
 
@@ -146,6 +130,11 @@ $script_name -i ORF_annotation_file -k .keg_file -o output -r single-copy-ref-ge
        as reference to calculate relative abundance. 
        e.g. there are 55 KOs M00178  Ribosome, bacteria [PATH:map03010] [BR:ko03011]
     -c cutoff , default 0.3, skip species unless with enough ref hits
-
+    -K key column (0'based) for key taxa, default 9 at species level,
+       8 will be at genus level
+       7 will be at family level
+       0 will be species level taxid
+       1 will be strain level taxid
+       10 will be strain
 EOD
 }
